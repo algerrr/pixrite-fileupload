017b407c9259211a93a2638856941c2f536b0416
Alger Brigham <alger.brigham@cayusetechnologies.com>
2020-07-01 13:41:02 -0700

    Fixing users API

--- src/app/_helpers/users.php
+++ src/app/_helpers/users.php
@@ -1,5 +1,10 @@
 <?php
 
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Credentials: true');
 // phpinfo();
 // $target_dir     = "/var/www/vhosts/3dintegrationgroup.com/httpdocs/secure-file-upload/uploads/";
 // $fileName       = basename($_FILES["fileToUpload"]["name"]);
@@ -22,7 +27,7 @@ $servername      = "localhost:3306";
 // $dbName         = "3dig_sfu";
 //dev Values
 $username       = "root";
-$password       = "";
+$password       = "admin";
 $dbName         = "pixrite";
 
 // Create connection
@@ -35,37 +40,38 @@ if ($conn->connect_error) {
 // print "Connected successfully";
 
 mysqli_select_db($conn, $dbName) or die( mysqli_error($conn) );
+$data = array();
 
 switch ($action) {
     case "findusers":
         echo "i is apple";
         break;
     case "getuploads":
-        $sql = "SELECT ID, TXN_STATUS FROM `upload_transaction` WHERE ID = (select MAX(id) from upload_transaction)";
+        $sql = "SELECT upload_transaction.ID AS TRANSACTION_ID, upload_transaction.TXN_STATUS, file_upload.PROJECT_TYPE, customers.CUSTOMER_NAME, customers.EMAIL, SUBSTRING(uploaded_files.FILE_URL,48) AS FILE_URL, upload_transaction.DATE_CREATED FROM file_upload INNER JOIN upload_transaction ON file_upload.TXN_ID = upload_transaction.ID INNER JOIN customers ON file_upload.CUST_ID = customers.ID INNER JOIN uploaded_files ON uploaded_files.FILE_UPLOAD_ID = file_upload.ID";
         $result = $conn->query($sql);
-        $txnId = "";
-        $txnStatus = "";
         
         if ($result->num_rows > 0) {
           // output data of each row
+          $data = ['OKAY'];
+
           while($row = $result->fetch_assoc()) {
-            // print "txnId: " . $row["ID"]. "txnStatus: " . $row["TXN_STATUS"]. "<br>";
-            $txnId = $row["ID"];
-            $txnStatus = $row["TXN_STATUS"];
+            // print_r($row);
+            array_push($data, 
+            array('transaction_id' => $row["TRANSACTION_ID"], 
+            'transaction_status' => $row["TXN_STATUS"], 
+            'project_type' => $row["PROJECT_TYPE"], 
+            'customer_name' => $row["CUSTOMER_NAME"], 
+            'email' => $row["EMAIL"], 
+            'file_url' => $row["FILE_URL"], 
+            'date_created' => $row["DATE_CREATED"]));
           }
         } else {
-          // print "0 results from upload_transaction";
+          $data = ['NO_ROWS'];
         }
-        if ($txnId == ""){
-          die("Transaction ID is null");
-        }
-        break;
-    case "cake":
-        echo "i is cake";
         break;
 }
 
-$data = [ 'txnId' => $txnId, 'txnStatus' => $txnStatus ];
+// $data = [ 'txnId' => $txnId, 'txnStatus' => $txnStatus ];
 header('Content-Type: application/json;charset=utf-8');
 echo json_encode($data);
 
